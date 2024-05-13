ce65c59893a386db5190b30aafb62d23adbeb194
Antonio Antonino <aantonino5@disroot.org>
2024-05-13 17:57:39 +0300

    Stage 2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                let resp = b"HTTP/1.1 200 OK\r\n\r\n";
+                stream
+                    .write_all(resp)
+                    .expect("Failed to send resp to client.");
             }
             Err(e) => {
                 println!("error: {}", e);
