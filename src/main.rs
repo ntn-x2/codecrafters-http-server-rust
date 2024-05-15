e0bfaf31c031d901ab8e5275e320f070dbbdc92b
Antonio Antonino <aantonino5@disroot.org>
2024-05-15 19:59:46 +0300

    Stage 3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::TcpListener,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -6,9 +9,33 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                let resp = b"HTTP/1.1 200 OK\r\n\r\n";
+                let string = {
+                    let mut buf = BufReader::new(&mut stream);
+                    let mut s = String::new();
+                    buf.read_line(&mut s).unwrap();
+                    s
+                };
+                let (request, _headers) = {
+                    let mut parts = string.split("\r\n");
+                    (
+                        parts.next().unwrap().to_owned(),
+                        parts.next().unwrap().split("\r\n"),
+                    )
+                };
+                let (_method, path, _http_version) = {
+                    let mut components = request.split_ascii_whitespace();
+                    (
+                        components.next().unwrap(),
+                        components.next().unwrap(),
+                        components.next().unwrap(),
+                    )
+                };
+                let res = match path {
+                    "/" => b"HTTP/1.1 200 OK\r\n\r\n".as_slice(),
+                    _ => b"HTTP/1.1 404 Not Found\r\n\r\n".as_slice(),
+                };
                 stream
-                    .write_all(resp)
+                    .write_all(res)
                     .expect("Failed to send resp to client.");
             }
             Err(e) => {
