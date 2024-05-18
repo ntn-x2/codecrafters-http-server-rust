d89eecb0c686a04d4ab3d0a914e7eb24f323d5bf
Antonio Antonino <aantonino5@disroot.org>
2024-05-18 16:08:46 +0300

    Stage 5

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,15 @@ use std::{
     net::TcpListener,
 };
 
-fn return_echo_response(echo_payload: &str) -> Vec<u8> {
+fn echo_response(echo_payload: &str) -> Vec<u8> {
     let payload_size = echo_payload.len();
     let response_body = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {payload_size}\r\n\r\n{echo_payload}");
+    response_body.into_bytes()
+}
+
+fn user_agent_response(user_agent: &str) -> Vec<u8> {
+    let payload_size = user_agent.len();
+    let response_body = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {payload_size}\r\n\r\n{user_agent}");
     response_body.into_bytes()
 }
 
@@ -15,18 +21,20 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                let string = {
-                    let mut buf = BufReader::new(&mut stream);
-                    let mut s = String::new();
-                    buf.read_line(&mut s).unwrap();
-                    s
-                };
-                let (request, _headers) = {
-                    let mut parts = string.split("\r\n");
-                    (
-                        parts.next().unwrap().to_owned(),
-                        parts.next().unwrap().split("\r\n"),
-                    )
+                let (request, mut headers) = {
+                    let mut buf_reader = BufReader::new(stream.try_clone().unwrap());
+                    let mut lines = Vec::new();
+                    loop {
+                        let mut tmp_buffer = String::new();
+                        buf_reader.read_line(&mut tmp_buffer).unwrap();
+                        let trimmed_tmp_buffer = tmp_buffer.trim();
+                        if trimmed_tmp_buffer.is_empty() {
+                            break;
+                        }
+                        lines.push(trimmed_tmp_buffer.to_string());
+                    }
+                    #[allow(clippy::unnecessary_to_owned)]
+                    (lines[0].clone(), lines[1..].to_owned().into_iter())
                 };
                 let (_method, path, _http_version) = {
                     let mut components = request.split_ascii_whitespace();
@@ -38,9 +46,13 @@ fn main() {
                 };
                 let res = match path {
                     "/" => b"HTTP/1.1 200 OK\r\n\r\n".to_vec(),
-                    s if s.starts_with("/echo/") => {
-                        return_echo_response(s.split_at("/echo/".len()).1)
+                    "/user-agent" => {
+                        let user_agent_header = headers
+                            .find_map(|h| h.strip_prefix("User-Agent: ").map(|s| s.to_string()))
+                            .unwrap();
+                        user_agent_response(&user_agent_header)
                     }
+                    s if s.starts_with("/echo/") => echo_response(s.split_at("/echo/".len()).1),
                     _ => b"HTTP/1.1 404 Not Found\r\n\r\n".to_vec(),
                 };
                 stream
